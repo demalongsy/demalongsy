067f1d005841dbdfd283fa460d41167b05b69573
bambionvacation <bammily25010@gmail.com>
2023-01-09 22:09:00 +0700

    add fontWeight

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -22,12 +22,12 @@ class _LonginState extends State<Longin> {
               const Spectral(
                 text: "Demalonsy",
                 color: C.primaryDefault,
-                fontWeight: FontWeight.bold,
+                fontWeight: FW.bold,
                 size: 40,
               ),
               const Button(
                 text: "Sign In",
-                fontWeight: FontWeight.bold,
+                fontWeight: FW.bold,
                 color: C.dark2,
                 size: 16,
                 boxColor: C.secondaryDefault,
