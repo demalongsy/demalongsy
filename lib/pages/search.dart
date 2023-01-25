b57d39df174a3560e57f047edd7341b55df52683
bambionvacation <bammily25010@gmail.com>
2023-01-25 22:00:11 +0700

    safe are for search page

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -26,81 +26,84 @@ class _SearchState extends State<Search> {
   }
 
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        backgroundColor: C.backgroundWhiteIvory,
-        appBar: AppBar(
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
           backgroundColor: C.backgroundWhiteIvory,
-          elevation: 0.0,
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(0),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 24, right: 24),
-              child: const InputField(
-                color: C.disableTextfield,
-                text: 'Search',
-                size: 16,
-                // boxHeight: 40,
+          appBar: AppBar(
+            backgroundColor: C.backgroundWhiteIvory,
+            elevation: 0.0,
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(0),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24, right: 24),
+                child: const InputField(
+                  color: C.disableTextfield,
+                  text: 'Search',
+                  size: 16,
+                  // boxHeight: 40,
+                ),
               ),
             ),
           ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 54),
-          child: Column(
-            children: [
-              TabBar(
-                  // controller: controller,
-                  labelColor: Colors.black,
-                  // labelPadding: const EdgeInsets.symmetric(horizontal: 2),
-                  indicator: UnderlineTabIndicator(
-                    borderSide: BorderSide(width: 2.0),
-                    insets: EdgeInsets.symmetric(horizontal: 90.0, vertical: 0),
-                  ),
-                  indicatorColor: C.dark1,
-                  labelStyle: TextStyle(fontWeight: FW.bold, fontSize: 14),
-                  tabs: [
-                    Tab(
-                      // icon: Icon(Icons.home),
-                      text: "Post",
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 54),
+            child: Column(
+              children: [
+                TabBar(
+                    // controller: controller,
+                    labelColor: Colors.black,
+                    // labelPadding: const EdgeInsets.symmetric(horizontal: 2),
+                    indicator: UnderlineTabIndicator(
+                      borderSide: BorderSide(width: 2.0),
+                      insets:
+                          EdgeInsets.symmetric(horizontal: 90.0, vertical: 0),
                     ),
-                    Tab(
-                      text: "Account",
+                    indicatorColor: C.dark1,
+                    labelStyle: TextStyle(fontWeight: FW.bold, fontSize: 14),
+                    tabs: [
+                      Tab(
+                        // icon: Icon(Icons.home),
+                        text: "Post",
+                      ),
+                      Tab(
+                        text: "Account",
+                      )
+                    ]),
+                Expanded(
+                  child: TabBarView(children: [
+                    Container(
+                      child: Center(
+                        child: Text("1st tab"),
+                      ),
+                    ),
+                    Container(
+                      child: Center(
+                        child: Text("2nd tab"),
+                      ),
                     )
                   ]),
-              Expanded(
-                child: TabBarView(children: [
-                  Container(
-                    child: Center(
-                      child: Text("1st tab"),
-                    ),
-                  ),
-                  Container(
-                    child: Center(
-                      child: Text("2nd tab"),
-                    ),
-                  )
-                ]),
-              )
-            ],
+                )
+              ],
+            ),
           ),
+          // body: Center(
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(23.0),
+          //     // child: Column(
+          //     //   // ignore: prefer_const_literals_to_create_immutables
+          //     //   children: [
+          //     //     const InputField(
+          //     //       // color: C.disableTextfield,
+          //     //       text: 'Search',
+          //     //       size: 16,
+          //     //     )
+          //     //   ],
+          //     // ),
+          //   ),
+          // ),
         ),
-        // body: Center(
-        //   child: Padding(
-        //     padding: const EdgeInsets.all(23.0),
-        //     // child: Column(
-        //     //   // ignore: prefer_const_literals_to_create_immutables
-        //     //   children: [
-        //     //     const InputField(
-        //     //       // color: C.disableTextfield,
-        //     //       text: 'Search',
-        //     //       size: 16,
-        //     //     )
-        //     //   ],
-        //     // ),
-        //   ),
-        // ),
       ),
     );
   }
