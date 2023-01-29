4a7d964b5fa882801ad666ba1ac9e616edb82e16
RemoNight <yositamook@gmail.com>
2023-01-29 22:31:51 +0700

    Add TextFormField

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -17,7 +17,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          backgroundColor: C.primaryDefault,
+          backgroundColor: C.white,
           appBar: PreferredSize(
               preferredSize: Size.fromHeight(50),
               child: AppBar(
@@ -48,7 +48,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                         ),
                         Expanded(child: Container()),
                         GestureDetector(
-                          child: Poppins(
+                          child: const Poppins(
                             text: "Save",
                             size: 16,
                             color: C.primaryDefault,
@@ -60,109 +60,61 @@ class _EditProfilePageState extends State<EditProfilePage> {
                 ),
               )),
           body: Container(
-            // padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+            padding: const EdgeInsets.only(left: 30, right: 30),
             child: GestureDetector(
               onTap: () {
                 FocusScope.of(context).unfocus();
               },
               child: ListView(
                 children: [
-                  // Text(
-                  //   "Edit Profile",
-                  //   style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-                  // ),
-                  SizedBox(
+                  const SizedBox(
                     height: 15,
                   ),
                   Center(
                     child: Stack(
                       children: [
                         Container(
-                          width: 130,
-                          height: 130,
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  width: 4,
-                                  color: Theme.of(context)
-                                      .scaffoldBackgroundColor),
-                              boxShadow: [
-                                BoxShadow(
-                                    spreadRadius: 2,
-                                    blurRadius: 10,
-                                    color: Colors.black.withOpacity(0.1),
-                                    offset: Offset(0, 10))
-                              ],
+                          width: 98,
+                          height: 98,
+                          decoration: const BoxDecoration(
                               shape: BoxShape.circle,
                               image: DecorationImage(
                                   fit: BoxFit.cover,
                                   image: NetworkImage(
-                                    "https://images.pexels.com/photos/3307758/pexels-photo-3307758.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250",
+                                    "https://s.isanook.com/wo/0/ui/38/190849/277150081_1212531879516407_7579357549109873866_n.jpg?ip/convert/w0/q80/jpg",
                                   ))),
                         ),
                         Positioned(
                             bottom: 0,
                             right: 0,
                             child: Container(
-                              height: 40,
-                              width: 40,
-                              decoration: BoxDecoration(
+                              height: 34,
+                              width: 34,
+                              decoration: const BoxDecoration(
                                 shape: BoxShape.circle,
-                                border: Border.all(
-                                  width: 4,
-                                  color:
-                                      Theme.of(context).scaffoldBackgroundColor,
-                                ),
-                                color: Colors.green,
+                                color: C.secondaryDefault,
                               ),
-                              child: Icon(
-                                Icons.edit,
-                                color: Colors.white,
+                              child: const Icon(
+                                Icons.mode_edit_rounded,
+                                color: C.dark2,
+                                size: 20,
                               ),
                             )),
                       ],
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextFieldUsername(),
+                  TextFieldName(""),
+                  TextFieldEmail("K. Payoungdech"),
+                  TextFieldBio("payoungdech.k@gmail.com"),
+                  // buildTextField("Email", "********"),
+                  // buildTextField("Bio", "TLV, Israel"),
+                  const SizedBox(
                     height: 35,
                   ),
-                  buildTextField("Full Name", "Dor Alex", false),
-                  buildTextField("E-mail", "alexd@gmail.com", false),
-                  buildTextField("Password", "********", true),
-                  buildTextField("Location", "TLV, Israel", false),
-                  SizedBox(
-                    height: 35,
-                  ),
-                  // Row(
-                  //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  //   children: [
-                  //     OutlineButton(
-                  //       padding: EdgeInsets.symmetric(horizontal: 50),
-                  //       shape: RoundedRectangleBorder(
-                  //           borderRadius: BorderRadius.circular(20)),
-                  //       onPressed: () {},
-                  //       child: Text("CANCEL",
-                  //           style: TextStyle(
-                  //               fontSize: 14,
-                  //               letterSpacing: 2.2,
-                  //               color: Colors.black)),
-                  //     ),
-                  //     RaisedButton(
-                  //       onPressed: () {},
-                  //       color: Colors.green,
-                  //       padding: EdgeInsets.symmetric(horizontal: 50),
-                  //       elevation: 2,
-                  //       shape: RoundedRectangleBorder(
-                  //           borderRadius: BorderRadius.circular(20)),
-                  //       child: Text(
-                  //         "SAVE",
-                  //         style: TextStyle(
-                  //             fontSize: 14,
-                  //             letterSpacing: 2.2,
-                  //             color: Colors.white),
-                  //       ),
-                  //     )
-                  //   ],
-                  // )
                 ],
               ),
             ),
@@ -172,36 +124,190 @@ class _EditProfilePageState extends State<EditProfilePage> {
     );
   }
 
-  Widget buildTextField(
-      String labelText, String placeholder, bool isPasswordTextField) {
+  Widget TextFieldUsername() {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 35.0),
-      child: TextField(
-        obscureText: isPasswordTextField ? showPassword : false,
-        decoration: InputDecoration(
-            suffixIcon: isPasswordTextField
-                ? IconButton(
-                    onPressed: () {
-                      setState(() {
-                        showPassword = !showPassword;
-                      });
-                    },
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      color: Colors.grey,
-                    ),
-                  )
-                : null,
-            contentPadding: EdgeInsets.only(bottom: 3),
-            labelText: labelText,
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            hintText: placeholder,
-            hintStyle: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            )),
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.multiline,
+            minLines: 1, //Normal textInputField will be displayed
+            maxLines: 5, // when user presses enter it will adapt to it
+
+            decoration: const InputDecoration(
+                icon: Poppins(
+                    text: "Username",
+                    size: 14,
+                    color: C.dark2,
+                    fontWeight: FW.light),
+                hintText: "     K. Payoungdech",
+                hintStyle: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  fontWeight: FW.light,
+                  color: C.disableTextfield,
+                )),
+            onSaved: (String? value) {
+              // This optional block of code can be used to run
+              // code when the user saves the form.
+            },
+            validator: (String? value) {
+              return (value != null && value.contains('@'))
+                  ? 'Do not use the @ char.'
+                  : null;
+            },
+          ),
+        ],
       ),
+      // SizedBox(
+      //   width: 20,
+      // ),
+    );
+  }
+
+  Widget TextFieldName(String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.multiline,
+            minLines: 1, //Normal textInputField will be displayed
+            maxLines: 5, // when user presses enter it will adapt to it
+
+            decoration: InputDecoration(
+                icon: const Poppins(
+                    text: "Name        ",
+                    size: 14,
+                    color: C.dark2,
+                    fontWeight: FW.light),
+                hintText:
+                    placeholder == "" ? "     Name" : "      " + placeholder,
+                hintStyle: placeholder != ""
+                    ? const TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 16,
+                        fontWeight: FW.light,
+                        color: C.textDefault,
+                      )
+                    : const TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 16,
+                        fontWeight: FW.light,
+                        color: C.disableTextfield,
+                      )),
+            onSaved: (String? value) {
+              // This optional block of code can be used to run
+              // code when the user saves the form.
+            },
+            validator: (String? value) {
+              return (value != null && value.contains('@'))
+                  ? 'Do not use the @ char.'
+                  : null;
+            },
+          ),
+        ],
+      ),
+      // SizedBox(
+      //   width: 20,
+      // ),
+    );
+  }
+
+  Widget TextFieldEmail(String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.multiline,
+            minLines: 1, //Normal textInputField will be displayed
+            maxLines: 5, // when user presses enter it will adapt to it
+
+            decoration: InputDecoration(
+                icon: Poppins(
+                    text: "Email         ",
+                    size: 14,
+                    color: C.dark2,
+                    fontWeight: FW.light),
+                hintText:
+                    placeholder == "" ? "     Email" : "     " + placeholder,
+                hintStyle: placeholder != ""
+                    ? const TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 16,
+                        fontWeight: FW.light,
+                        color: C.textDefault,
+                      )
+                    : const TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 16,
+                        fontWeight: FW.light,
+                        color: C.disableTextfield,
+                      )),
+            onSaved: (String? value) {
+              // This optional block of code can be used to run
+              // code when the user saves the form.
+            },
+            validator: (String? value) {
+              return (value != null && value.contains('@'))
+                  ? 'Do not use the @ char.'
+                  : null;
+            },
+          ),
+        ],
+      ),
+      // SizedBox(
+      //   width: 20,
+      // ),
+    );
+  }
+
+  Widget TextFieldBio(String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.multiline,
+            // minLines: 1, //Normal textInputField will be displayed
+            maxLines: 5, // when user presses enter it will adapt to it
+
+            decoration: InputDecoration(
+                icon: Poppins(
+                    text: "Bio              ",
+                    size: 14,
+                    color: C.dark2,
+                    fontWeight: FW.light),
+                hintText: placeholder == "" ? "     Bio" : "    " + placeholder,
+                hintStyle: placeholder != ""
+                    ? const TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 16,
+                        fontWeight: FW.light,
+                        color: C.textDefault,
+                      )
+                    : const TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 16,
+                        fontWeight: FW.light,
+                        color: C.disableTextfield,
+                      )),
+            onSaved: (String? value) {
+              // This optional block of code can be used to run
+              // code when the user saves the form.
+            },
+            validator: (String? value) {
+              return (value != null && value.contains('@'))
+                  ? 'Do not use the @ char.'
+                  : null;
+            },
+          ),
+        ],
+      ),
+      // SizedBox(
+      //   width: 20,
+      // ),
     );
   }
 }
